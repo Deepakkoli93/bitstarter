6076309c76bfea7b65c44127f1b07bcf851e85f9
Deepak Koli <Deepakkoli93@gmail.com>
2013-07-09 23:38:04 +0530

    index into web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  
+ var fs = require('fs');
   var buf = new Buffer(256);
   buf = fs.readFileSync('index.html', function (err, data) {
   if (err) throw err;
