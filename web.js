3132534cf354f19e3d45cebb523ab71c6c158b2a
Deepak Koli <Deepakkoli93@gmail.com>
2013-07-10 12:38:29 +0530

    Added

--- web.js
+++ web.js
@@ -2,17 +2,18 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-/*app.get('/', function(request, response) {
- var fs = require('fs');
-  var buf = new Buffer(256);
- buf = fs.readFileSync('index.html');
+app.get('/', function(request, response) {
+// var fs = require('fs');
+ // var buf = new Buffer(256);
+ //buf = fs.readFileSync('index.html');
 
-  response.send(buf.toString("utf-8",0,27));
-});*/
-
-app.get("/", function(req, res) {
-  render("index.html");
+ // response.send(buf.toString("utf-8",0,27));
+ response.send('index.html');
 });
+
+/*app.get("/", function(req, res) {
+  render("index.html");
+});*/
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
