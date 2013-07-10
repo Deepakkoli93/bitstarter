d50e698e81bc0df44c1937cff6a6d6e28493e0b8
Deepak Koli <Deepakkoli93@gmail.com>
2013-07-10 13:29:36 +0530

    Added

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
+app.get('/index.html', function(request, response) {
 // var fs = require('fs');
  // var buf = new Buffer(256);
  //buf = fs.readFileSync('index.html');
