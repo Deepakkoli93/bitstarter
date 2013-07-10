a2aff3ac23897ee2f0fe33b9a94f48c7506c04d8
Deepak Koli <Deepakkoli93@gmail.com>
2013-07-10 12:35:53 +0530

    Added basic layout

--- web.js
+++ web.js
@@ -2,15 +2,20 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
+/*app.get('/', function(request, response) {
  var fs = require('fs');
   var buf = new Buffer(256);
  buf = fs.readFileSync('index.html');
 
   response.send(buf.toString("utf-8",0,27));
+});*/
+
+app.get("/", function(req, res) {
+  render("index.html");
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
